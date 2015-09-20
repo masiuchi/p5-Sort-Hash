78b6cb46300933e3c3dc92d0270ac636504344f3
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2015-09-20 16:14:48 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Sort::Hash - It's new $module
+
+# SYNOPSIS
+
+    use Sort::Hash;
+
+# DESCRIPTION
+
+Sort::Hash is ...
+
+# LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Masahiro Iuchi &lt;masahiro.iuchi@gmail.com>
---
+++ lib/Sort/Hash.pm
@@ -0,0 +1,39 @@
+package Sort::Hash;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sort::Hash - It's new $module
+
+=head1 SYNOPSIS
+
+    use Sort::Hash;
+
+=head1 DESCRIPTION
+
+Sort::Hash is ...
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masahiro Iuchi E<lt>masahiro.iuchi@gmail.comE<gt>
+
+=cut
+
